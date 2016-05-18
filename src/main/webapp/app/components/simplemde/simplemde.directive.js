dcb60373411889ff1f45063f9e51a9ea0aebc085
daiyungui <yungui.dai@pactera.com>
2016-05-18 20:59:15 +0800

    增加编辑markdown显示

--- src/main/webapp/app/components/simplemde/simplemde.directive.js
+++ src/main/webapp/app/components/simplemde/simplemde.directive.js
@@ -2,26 +2,35 @@
   'use strict';
   angular
     .module('demoApp')
-    .controller('fndController', ['$scope', function($scope) {
+    .controller('fndController', ['$scope','$parse', function($scope,$parse) {
       $scope.value = 'Hello';
     }])
-    .directive('fndMde', function() {
+    .directive('fndMde', function($timeout) {
       return {
         restrict: 'E',
         transclude: true,
         require: '?ngModel',
         scope: {},
         template: '<textarea></textarea>',
-        link: function(scope, element, attrs, ngModel) {
+        link: function(scope, element, attrs, ngModelCtrl) {//https://www.nadeau.tv/using-ngmodelcontroller-with-custom-directives/
           console.log("simplemde");
           var simplemde = new SimpleMDE();
-          //var value = ngModel.$viewValue
+          //var value = ngModelCtrl.$viewValue;
           //simplemde.value(value);
+          $timeout(function() {
+                    var value = ngModelCtrl.$viewValue;
+                    simplemde.value(value);
+                }, 0);
+
           simplemde.codemirror.on("change", function(){
               console.log(simplemde.value());
-              ngModel.$setViewValue(simplemde.value());
+              ngModelCtrl.$setViewValue(simplemde.value());
               scope.$apply();
           });
+          scope.modelValue = function () {
+                console.log('modelvalue - ' + ngModelCtrl.$viewValue);
+                return ngModelCtrl.$viewValue;
+            };
           //Todo Get and update editor value
         }
       };
