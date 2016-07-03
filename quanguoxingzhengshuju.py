8e98d712a7bc3042a1eeac94c554d2924af15fb8
daiyungui <yungui.dai@pactera.com>
2016-07-03 13:12:14 +0800

    增加城市数据库抓取

--- quanguoxingzhengshuju.py
+++ quanguoxingzhengshuju.py
@@ -1,21 +1,40 @@
 #coding:utf-8
+from pypinyin import pinyin, lazy_pinyin
+import pypinyin
 import csv
 
-f = open('//Users//daiyungui//Downloads//全国行政数据', 'r')
+def pinyinFirst(name):
+    print name
+    pinyin = lazy_pinyin(name)
+    retPinyin = []
+    for element in pinyin:
+        retPinyin =retPinyin + [element[0]]
+
+    return retPinyin
+
+def listToUtf8(list):
+    str = ''
+    for element in list:
+        str =str+element+'\''
+    str = str[:-1]    
+    return str.encode("UTF-8")
+
+f = open('全国行政数据', 'r')
 provinceCode = ''
 city = ''
+pinyin = ''
 with open('province.csv','w') as csvfile:
     spamwriter = csv.writer(csvfile,delimiter=';')
-    spamwriter.writerow(['id','province_id', 'province_name'])
+    spamwriter.writerow(['id','province_id', 'province_name','pinyin','pinyin_first'])
 
 with open('city.csv','w') as csvfile:
     spamwriter = csv.writer(csvfile,delimiter=';')
-    spamwriter.writerow(['id','province_id','city_id', 'city_name'])
+    spamwriter.writerow(['id','province_id','city_id', 'city_name','pinyin','pinyin_first'])
 
 result = list()
 provCount = 0
 cityCount = 0
-for line in open('//Users//daiyungui//Downloads//全国行政数据'):
+for line in open('全国行政数据'):
     line = f.readline()
     line=line.strip('\n')
     if len(line) != 0:
@@ -33,7 +52,7 @@ for line in open('//Users//daiyungui//Downloads//全国行政数据'):
             print('省/自治区编号：'+code.decode('string_escape')+' 名称:'+name)
             with open('province.csv','a') as csvfile:
                 spamwriter = csv.writer(csvfile,delimiter=';')
-                spamwriter.writerow([provCount,code, name.strip('\n')])
+                spamwriter.writerow([provCount,code, name.strip('\n'),listToUtf8(lazy_pinyin(unicode(name.strip('\n'),"utf-8"))),listToUtf8(pinyinFirst(unicode(name.strip('\n'),"utf-8")))])
 
         elif (num%1000)==0 :
             pass
@@ -48,7 +67,7 @@ for line in open('//Users//daiyungui//Downloads//全国行政数据'):
             print('省直辖县级行政区划编号：'+line.decode('string_escape')+' 名称:'+name)
             with open('city.csv','a') as csvfile:
                 spamwriter = csv.writer(csvfile,delimiter=';')
-                spamwriter.writerow([cityCount,provCount,num, name.strip('\n')])
+                spamwriter.writerow([cityCount,provCount,num, name.strip('\n'),listToUtf8(lazy_pinyin(unicode(name.strip('\n'),"utf-8"))),listToUtf8(pinyinFirst(unicode(name.strip('\n'),"utf-8")))])
         elif (num%100)==0:
             pass
             code = line
@@ -62,7 +81,7 @@ for line in open('//Users//daiyungui//Downloads//全国行政数据'):
             print('地级市编号：'+line.decode('string_escape')+' 名称:'+name)
             with open('city.csv','a') as csvfile:
                 spamwriter = csv.writer(csvfile,delimiter=';')
-                spamwriter.writerow([cityCount,provCount,num, name.strip('\n')])
+                spamwriter.writerow([cityCount,provCount,num, name.strip('\n'),listToUtf8(lazy_pinyin(unicode(name.strip('\n'),"utf-8"))),listToUtf8(pinyinFirst(unicode(name.strip('\n'),"utf-8")))])
         else:
             pass
             code = line
@@ -76,7 +95,7 @@ for line in open('//Users//daiyungui//Downloads//全国行政数据'):
             print('县级市编号：'+code.decode('string_escape')+' 名称:'+name)
             with open('city.csv','a') as csvfile:
                 spamwriter = csv.writer(csvfile,delimiter=';')
-                spamwriter.writerow([cityCount,provCount,num, name.strip('\n')])
+                spamwriter.writerow([cityCount,provCount,num, name.strip('\n'),listToUtf8(lazy_pinyin(unicode(name.strip('\n'),"utf-8"))),listToUtf8(pinyinFirst(unicode(name.strip('\n'),"utf-8")))])
     else:
         break
 
